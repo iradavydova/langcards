f20c85b979bd915056e93c70d927d579bd53e3ba
iradavydova <irinadavydovawork@gmail.com>
2022-04-24 20:32:31 +0200

    First structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Wordslist from './components/Wordslist';
+import Searchbar from './components/Searchbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
       </header>
+      <div className="Search-bar">
+        <Searchbar />
+      </div>
+      <div className="Words-list">
+        <Wordslist />
+      </div>
+
+      <footer className="App-footer">
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ src/components/Wordslist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './card'
+
+function Wordslist() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Wordslist
